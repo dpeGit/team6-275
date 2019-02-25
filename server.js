dcc7432253d890566184032a438081c4b7ee89d2
Matthew Rademaekers <mmr337@drexel.edu>
2019-02-25 12:42:48 -0500

    Installed path to ensure css displays properly Additional deleted the older server file

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = 3000;
+const port = 3000; // Our proxy port
+
+app.use(express.static(path.join(__dirname, 'client'))); // add the client folder to the path
 
 app.get('/', function (req, res){
 		console.log('Recieved'); res.sendFile('index.html')
 });
 
-app.listen(port, ()	=> console.log('Listening on ' + port));
+app.listen(port, () => console.log('Listening on ' + port));
